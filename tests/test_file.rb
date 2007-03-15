0e0ec41f8b5bff13e07998602d093094564a866f
David Lutterkort <dlutter@redhat.com>
2007-03-15 04:18:10 +0000

    Fix segv when link_to is NIL

--- tests/test_file.rb
+++ tests/test_file.rb
@@ -2,11 +2,12 @@ require 'test/unit'
 require 'rpm'
 
 class RPM_File_Tests < Test::Unit::TestCase
-    def test_link_to_nil
-        # FIXME: This currently segfaults
-        assert_nothing_raised {
-            f = RPM::File.new("path", "md5sum", nil, 42, 1, 
-                              "owner", "group", 43, 0777, 44, 45)
-        }
+    def test_link_to
+        f = RPM::File.new("path", "md5sum", nil, 42, 1, 
+                          "owner", "group", 43, 0777, 44, 45)
+        assert_equal(nil, f.link_to)
+        f = RPM::File.new("path", "md5sum", "link_to", 42, 1, 
+                          "owner", "group", 43, 0777, 44, 45)
+        assert_equal("link_to", f.link_to)
     end
 end
