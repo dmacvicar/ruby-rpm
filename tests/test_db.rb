8369b233a499d0ca187a0099cf4516eb3338b0e1
Duncan Mac-Vicar P <dmacvicar@suse.de>
2009-11-10 17:41:06 +0100

    root and home attributes only available in rpm < 4.6.0

--- tests/test_db.rb
+++ tests/test_db.rb
@@ -3,8 +3,14 @@ require 'fileutils'
 require 'tmpdir'
 require 'rpm'
 
+def rpm_version(ver=[])
+  ver = `LANG=C rpm --version| cut -d' ' -f 3`.split(/\./) if ver.empty?
+  return (ver[0].to_i<<16) + (ver[1].to_i<<8) + (ver[2].to_i<<0)
+end
+
 class RPM_DB_Tests < Test::Unit::TestCase
   def setup
+    @version = rpm_version
     @work_dir = "#{Dir.tmpdir}/.ruby-rpm-test"
     unless File.directory?(@work_dir) then
       Dir.mkdir(@work_dir, 0777)
@@ -35,12 +41,18 @@ class RPM_DB_Tests < Test::Unit::TestCase
     assert( @a )
   end
 
-  def test_root
-    assert_equal( @a.root, @work_dir )
+  def test_root    
+    assert_equal( @a.root, @work_dir ) if @version < rpm_version([4,6,0])
+    if @version >= rpm_version([4,6,0])
+      assert_raise(NoMethodError) { @a.root }      
+    end
   end
 
   def test_home
-    assert_equal( @a.home, RPM['_dbpath'] )
+    assert_equal( @a.home, RPM['_dbpath'] ) if @version < rpm_version([4,6,0])
+    if @version >= rpm_version([4,6,0])
+      assert_raise(NoMethodError) { @a.home }
+    end
   end
 
 end # class RPM_DB_Tests < Test::Unit::TestCase
